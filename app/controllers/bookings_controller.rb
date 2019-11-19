eb0dce4673b97defd1d2870bef879d0e9fefd7d7
Rutuja Deshkar <rutuja.deshkar@gmail.com>
2019-11-19 15:38:56 +0100

    Adds delete logic on spaces index page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,6 +2,7 @@ class BookingsController < ApplicationController
   before_action :set_booking, only: [:show, :edit, :update, :destroy]
 
   def index
+    @bookings = Booking.all
   end
 
   def show
