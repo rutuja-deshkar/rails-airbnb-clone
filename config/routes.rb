70c1027b8866710263e20af79858c73edad14a1b
Rutuja Deshkar <rutuja.deshkar@gmail.com>
2019-11-21 12:29:57 +0100

    View all bookings for a space, fixs signup, change routes for all bookings

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :spaces do
-    resources :bookings, only: [:new, :create]
+    resources :bookings, only: [:new, :create, :index]
   end
-  resources :bookings, only: [:show, :index, :edit, :update, :destroy]
+  resources :bookings, only: [:show, :edit, :update, :destroy]
   get '/profile',  to: 'profiles#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
